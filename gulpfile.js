9712b83e84ed090d0abf6b97faa71067885554ad
Lukas Eibensteiner <l.eibensteiner@gmail.com>
2016-01-01 20:51:01 +0100

    Minify CSS and JavaScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require("gulp");
-var sass = require("gulp-sass");
 var browserSync = require("browser-sync");
+var gulpIf = require("gulp-if");
+var minifyCss = require("gulp-minify-css");
+var sass = require("gulp-sass");
+var uglify = require("gulp-uglify");
+var useref = require("gulp-useref");
 
 gulp.task("sass", function() {
   return gulp.src("web/scss/**/*.scss")
@@ -9,6 +13,14 @@ gulp.task("sass", function() {
     .pipe(browserSync.reload({
       stream: true
     }));
+});
+
+gulp.task("useref", function(){
+  return gulp.src("web/*.html")
+    .pipe(gulpIf("*.css", minifyCss()))
+    .pipe(gulpIf("*.js", uglify()))
+    .pipe(useref())
+    .pipe(gulp.dest("dist"));
 });
 
 gulp.task("browserSync", function() {
