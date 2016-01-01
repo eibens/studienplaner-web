98b473fcbc3bed3c11c8a4dbf81ac7fa25b6bcf0
Lukas Eibensteiner <l.eibensteiner@gmail.com>
2016-01-01 20:31:51 +0100

    Add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var browserSync = require("browser-sync");
 
 gulp.task("sass", function() {
   return gulp.src("web/scss/**/*.scss")
     .pipe(sass())
     .pipe(gulp.dest("web/css"))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task("watch", function(){
+gulp.task("browserSync", function() {
+  browserSync({
+    server: {
+      baseDir: "web"
+    }
+  });
+});
+
+gulp.task("watch", ["browserSync", "sass"], function(){
   gulp.watch("web/scss/**/*.scss", ["sass"]);
 });
