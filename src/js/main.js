2bd3c1112d9de0b8e891a1e573c56e116e537e2d
Lukas Eibensteiner <l.eibensteiner@gmail.com>
2016-01-16 17:09:52 +0100

    Show toolbar dynamically on homepage

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,14 @@ $(".zoomable").on("click", function (e) {
   e.preventDefault();
   $(this).find("img").toggleClass("zoomed");
 });
+
+var body = $("body");
+body.addClass("top");
+$(document).scroll(function () {
+  var scrollTop = $(document).scrollTop();
+  if (scrollTop == 0) {
+    body.addClass("top");
+  } else {
+    body.removeClass("top");
+  }
+});
